5623bbc40b201f5746022f4ef7da6a758bd32c58
kaosbeat <kasper.jordaens@gmail.com>
2018-05-20 10:44:35 +0200

    perlin noise for better random data

--- roboarchsim/scripts/init_arm.py
+++ roboarchsim/scripts/init_arm.py
@@ -17,24 +17,7 @@ def print_pos(pose):
     
 
 def probedata(probe):
-    print("probing %s" % probe)
-    # bundle = osc_bundle_builder.OscBundleBuilder(osc_bundle_builder.IMMEDIATELY)
-    # msg = osc_message_builder.OscMessageBuilder(address="/probe/data")
-    # msg.add_arg(probe['x'])
-    # bundle.add_content(msg.build())
-    # msg.add_arg(probe['y'])
-    # bundle.add_content(msg.build())
-    # msg.add_arg(probe['z'])
-    # bundle.add_content(msg.build())
-    # msg.add_arg(probe['distance'])
-    # bundle.add_content(msg.build())
-    # msg.add_arg(probe['probevalue'])
-    # bundle.add_content(msg.build())
-    # msg.add_arg(probe['color'])
-    # bundle.add_content(msg.build())
-    # bundle = bundle.build()
-    # print(dir(client))
-    # client.send(msg)
+    # print("probing %s" % probe)
     client.send_message("/probe/x", probe['x'])
     client.send_message("/probe/y", probe['y'])
     client.send_message("/probe/z", probe['z'])
@@ -42,29 +25,15 @@ def probedata(probe):
     client.send_message("/probe/value", probe['probevalue'])
     client.send_message("/probe/color", probe['color'])
 
-def depthdata(data):
-    print(data['points'])
-    # v = memoryview(data['points'].encode())
-    # print(bytes(v[0:16]))
-    # print(v)  ####<memory at 0x107f21108>
-    # x = np.fromstring(data['points'], dtype='>f2')
-    # print(x)
-
-    # print(data)
-    # print(struct.unpack_from('f', data['points'].encode(), 12 )) ##(9.126204533022451e+23,)
-    # print(struct.unpack('f', data['points'].encode()))
-    # print("test")
-
 
 def startScan():
     with pymorse.Morse() as simu:
         simu.robot.arm.probeviz.subscribe(probedata)
-
         # print(simu.robot.arm.depthvideocamera.get_properties())
         # print(dir(simu.robot.arm))
         # print(simu.robot.pa10.get_joints())
-        # print(simu.robot.arm.get_IK_limits("kuka_1"))
-        # print(simu.robot.arm.get_IK_limits("kuka_2"))
+        print(simu.robot.arm.get_IK_limits("kuka_1"))
+        print(simu.robot.arm.get_IK_limits("kuka_2"))
         # move_IK_target(name, translation, rotation, relative, linear_speed, radial_speed) (non blocking)
         for x in range(100):
             # simu.robot.arm.move_IK_target('ik_target.robot.arm.kuka_7', [0.2,0.05*x,0.7], [0.0,0.0,0.0], False, 1,1 )
@@ -94,6 +63,11 @@ def set_horizontal():
         simu.robot.arm.depthvideocamera.capture(-1)
         simu.robot.arm.depthvideocamera.subscribe(depthdata)
         
+
+def jump_to_new_scan():
+    with pymorse.Morse() as simu:
+        simu.robot.arm.set_rotation("kuka_2", x*0.01)
+        simu.robot.arm.set_rotation("kuka_1", y*0.01)
 
 
 # def probe_value():
